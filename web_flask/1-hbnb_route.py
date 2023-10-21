c40486358c89321b7a7e98fb8cae49eea4664c7b
Eric Ubaga <ubaga91@gmail.com>
2023-10-21 17:09:01 +0300

    added files 1-hbnb_route.py and 2-c_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,6 +3,7 @@
 script that starts a Flask web application
     listen on 0.0.0.0, port 5000
     routes: /:     display "Hello HBNB!"
+            /hbnb: display "HBNB"
 """
 from flask import Flask
 app = Flask(__name__)
@@ -13,6 +14,12 @@ app.url_map.strict_slashes = False
 def hello():
     """ Initialize content in the root page """
     return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ Initialize content in the hbnb page """
+    return "HBNB!"
 
 
 if __name__ == '__main__':
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,6 +3,9 @@
 script that starts a Flask web application
     listen on 0.0.0.0, port 5000
     routes: /:     display "Hello HBNB!"
+            /hbnb: display "HBNB"
+            /c/<text>: display “C ” followed by the value of
+            the text variable
 """
 from flask import Flask
 app = Flask(__name__)
@@ -13,6 +16,19 @@ app.url_map.strict_slashes = False
 def hello():
     """ Initialize content in the root page """
     return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ Initialize content in the hbnb page """
+    return "HBNB!"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """ Initialize content in the c/ page """
+    my_text = text.replace('_', ' ')
+    return "C {}".format(my_text)
 
 
 if __name__ == '__main__':
