d2983a10cfc0430d43775d60b4af0250f3d9d171
Eric Ubaga <ubaga91@gmail.com>
2023-10-22 16:29:35 +0300

    added task 9 files

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,7 @@
 script that starts a Flask web application
 listen on 0.0.0.0, port 5000
 routes: /states_list:         display HTML and state info from storage
+        /cities_by_states     display HTML with cities in state info
 """
 from flask import Flask, render_template
 from models import storage, State
@@ -26,6 +27,15 @@ def states_list():
     """
     state_objs = list(storage.all("State").values())
     return render_template('7-states_list.html', state_objs=state_objs)
+
+
+@app.route('/cities_by_states')
+def cities_by_states_list():
+    """
+    Display a html page of all cities by States in DB ordered by Name
+    """
+    state_objs = list(storage.all("State").values())
+    return render_template('8-cities_by_states.html', state_objs=state_objs)
 
 
 if __name__ == '__main__':
