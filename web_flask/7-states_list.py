419ef624bca713c309f763eeb41deeba6eb22634
Eric Ubaga <ubaga91@gmail.com>
2023-10-22 15:51:33 +0300

    corrected file 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,18 +11,23 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    """
-    Display a html page of all States in DB ordered by Name
-    """
-    state_objs = [state for state in storage.all("State").values()]
-    return render_template('7-states_list.html', state_objs=state_objs)
-
-
 @app.teardown_appcontext
 def teardown_session():
     """
     terminate an SQLAlchemy session
     """
     storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """
+    Display a html page of all States in DB ordered by Name
+    """
+    state_objs = [state for state in storage.all("State").values()]
+    print(state_objs)
+    return render_template('7-states_list.html', state_objs=state_objs)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
