4db8fec64d639d7cc2e00c860be5d4ac3e46a54b
Eric Ubaga <ubaga91@gmail.com>
2023-10-22 17:02:07 +0300

    corrected time initialization in base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -12,43 +12,30 @@ class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        # if kwargs:
-        #     for key, value in kwargs.items():
-        #         if key != '__class__':
-        #             if hasattr(self, key):
-        #                 setattr(self, key, value)
-        #             else:
-        #                 raise KeyError
-        #     if 'id' not in kwargs:
-        #         setattr(self, 'id', str(uuid.uuid4()))
-        # else:
-        #     self.id = str(uuid.uuid4())
-        #     self.created_at = datetime.utcnow()
-        #     self.updated_at = datetime.utcnow()
-        if not kwargs:
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
+            if kwargs.get("created_at"):
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("created_at"):
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
             for key, val in kwargs.items():
-                if key == 'updated_at' or key == 'created_at':
-                    val = datetime.strptime(val,
-                                            '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    if hasattr(self, key):
-                        setattr(self, key, val)
-                    else:
-                        raise KeyError
-            if 'id' not in kwargs:
-                setattr(self, 'id', str(uuid.uuid4()))
-            if 'created_at' not in kwargs:
-                setattr(self, 'created_at', datetime.utcnow())
-            if 'updated_at' not in kwargs:
-                setattr(self, 'updated_at', datetime.utcnow())
+                if "__class__" not in key:
+                    setattr(self, key, val)
+            if not self.id:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
